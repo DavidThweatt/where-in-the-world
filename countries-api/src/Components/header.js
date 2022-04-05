c31074058a3ff67fdc99cb5ce78d9176dda5e55b
David Thweatt <68977760+DavidThweatt@users.noreply.github.com>
2022-04-05 15:58:05 -0700

    header styles
    created dark mode button with moon svg, added title and background and font colors

--- countries-api/src/Components/header.js
+++ countries-api/src/Components/header.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "../Styles/header.css";
+import moon from "../images/moon-fill.svg";
 
 export default function Header() {
   return (
-    <header className="level is-mobile">
-      <div className="level-left m-2">
-        <h1 className="level-item is-size-4">Where in the world?</h1>
-      </div>
-      <div className="level-right m-2">
-        <button className="level-item">Dark Mode</button>
+    <header className="container">
+      <div className="level p-3 is-mobile">
+        <div className="level-left m-2">
+          <h1 className="level-item is-size-4 has-text-weight-bold has-text-white">
+            Where in the world?
+          </h1>
+        </div>
+        <div className="level-right m-2">
+          <button className="level-item button is-size-5 has-text-weight-medium has-text-white p-0 m-0">
+            <figure className="image is-24x24 mr-2">
+              <img src={moon} alt="moon dark mode" className="moon" />
+            </figure>
+            Dark Mode
+          </button>
+        </div>
       </div>
     </header>
   );
